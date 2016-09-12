7e9c5761b16c382f64d49c4346fc63b37e416396
Dmitriy Bizyaev <bizyaev@ordbuy.com>
2016-09-12 13:52:20 +0300

    minor fix

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const start = () => co(function* () {
     setupEndpoint(app, require('./endpoints/update-project'));
 
     if (config.get('env') === 'development') {
-        // setupWebpackDevMiddleware(app);
+        setupWebpackDevMiddleware(app);
     }
 
     httpServer = http.createServer(app);
