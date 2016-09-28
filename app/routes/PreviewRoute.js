db8f570107d94f4de078d31d09d4cd1bbbe99602
Dmitriy Bizyaev <bizyaev@ordbuy.com>
2016-09-28 15:51:21 +0300

    fixed preview route

--- app/routes/PreviewRoute.js
+++ app/routes/PreviewRoute.js
@@ -1,23 +1,18 @@
 'use strict';
 
+//noinspection JSUnresolvedVariable
 import React, { Component } from 'react';
 
 import { PreviewIFrame } from '../components/PreviewIFrame/PreviewIFrame';
 import store from '../store';
 
-class PreviewRoute extends Component {
+export default class PreviewRoute extends Component {
     render() {
-        const _src = `/preview/${this.props.params.projectName}/index.html`;
-
         return (
             <PreviewIFrame
-                externalStore={store}
-                canSelected={false}
-                canHighlight={false}
-                previewAppURL={_src}
+                store={store}
+                url={`/preview/${this.props.params.projectName}/index.html`}
             />
         );
     }
 }
-
-export default PreviewRoute;
