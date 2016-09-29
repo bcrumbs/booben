44ab4981fa02d2ef8916f7f40834a38ccc477c4f
katya marova <marova.ekaterina@gmail.com>
2016-09-29 19:45:09 +0300

    eTXG5JXM: props list init

---
+++ app/components/PropsList/PropsItem/PropsItem.js
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react';
+
+export const PropsItem = props => {
+    let className = 'props-item';
+
+    return (
+        <div className={className}>
+
+        </div>
+    );
+};
+
+PropsItem.propTypes = {
+};
+
+PropsItem.defaultProps = {
+};
+
+PropsItem.displayName = 'PropsItem';
---
+++ app/components/PropsList/PropsList.js
@@ -0,0 +1,21 @@
+import './PropsList.scss'
+
+import React, { PropTypes } from 'react';
+
+export const PropsList = props => {
+    let className = 'props-list';
+
+    return (
+        <div className={className}>
+
+        </div>
+    );
+};
+
+PropsList.propTypes = {
+};
+
+PropsList.defaultProps = {
+};
+
+PropsList.displayName = 'PropsList';
