82cb31ad82daef82371e7558b15988530d9149dc
Dmitriy Bizyaev <dmitriy.bizyaev@gmail.com>
2016-11-03 16:58:50 +0300

    minor improvement

--- app/store.js
+++ app/store.js
@@ -9,10 +9,13 @@ import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from './reducers';
 
-const loggerMiddleware = createLogger();
+const middleware = [thunkMiddleware];
+
+if (process.env.NODE_ENV === 'development')
+    middleware.push(createLogger());
 
 const fns = [
-    applyMiddleware(thunkMiddleware, loggerMiddleware)
+    applyMiddleware(...middleware)
 ];
 
 const willAddReduxDevTools =
