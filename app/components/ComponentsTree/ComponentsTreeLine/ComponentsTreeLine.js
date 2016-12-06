426b11c43e386aab2181181b60f9746ae4621711
katya marova <marova.ekaterina@gmail.com>
2016-12-06 19:32:18 +0300

    ComponentTreeLine

--- app/components/ComponentsTree/ComponentsTreeLine/ComponentsTreeLine.js
+++ app/components/ComponentsTree/ComponentsTreeLine/ComponentsTreeLine.js
@@ -1,21 +1,13 @@
 import React from 'react';
 
 export const ComponentsTreeLine = ({ createElementRef }) =>
-	// TODO Create normal line please :C
 	<div
 		ref={createElementRef}
-		className="components-tree-item"
-		style={{height: '4px', marginBottom: '-8px'}}
+		className="components-tree_line-wrapper"
 	>
-		<div
-			className="components-tree-item-content"
-			style={{ height: 'inherit' }}
-		>
-			<div
-				className="components-tree-item-title-wrapper"
-				style={{ backgroundColor: 'red', height: 'inherit', width: '100%' }}
-			/>
-		</div>
+        <div
+            className="components-tree_line"
+        />
 	</div>;
 
 ComponentsTreeLine.PropTypes = {
