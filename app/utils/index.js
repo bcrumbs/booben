f4691a181f2dc708e277771d470dd66484115fdd
Dmitriy Bizyaev <bizyaev@ordbuy.com>
2016-12-13 18:04:49 +0300

    after-merge fix

--- app/utils/index.js
+++ app/utils/index.js
@@ -30,7 +30,7 @@ export const getLocalizedText = (localization, language, id, values = {}) =>
 /**
   *
   * @param {Object} state
-  * @return {Function<string, string>:string}
+  * @return {function(string, string): string}
   */
 export const getLocalizedTextFromState = state =>
 	localizationFuncsMap.get(state.app.localization)
