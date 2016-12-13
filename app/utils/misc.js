d72b36e6f1c973d5d6f590947b341590b614710f
Dmitriy Bizyaev <bizyaev@ordbuy.com>
2016-12-13 17:46:59 +0300

    after-merge fix

--- app/utils/misc.js
+++ app/utils/misc.js
@@ -16,12 +16,72 @@ export const returnNull = /* istanbul ignore next */ () => null;
 
 /**
  *
+ * @param {*} maybeObject
+ * @return {boolean}
+ */
+export const isObject = maybeObject =>
+    typeof maybeObject === 'object' && maybeObject !== null;
+
+/**
+ *
+ * @param {Object} object
+ * @param {function(value: *, key: string, object: Object)} fn
+ */
+export const objectForEach = (object, fn) =>
+    void Object.keys(object).forEach(key => void fn(object[key], key, object));
+
+/**
+ *
+ * @param {Object} object
+ * @param {function(value: *, key: string, object: Object)} fn
+ * @return {Object}
+ */
+export const objectMap = (object, fn) => {
+    const ret = {};
+    objectForEach(object, (value, key, object) => {
+        ret[key] = fn(value, key, object);
+    });
+    return ret;
+};
+
+/**
+ *
  * @param {Object} object
  * @param {function(value: *, key: string, object: Object): boolean} predicate
  * @return {boolean}
  */
 export const objectSome = (object, predicate) =>
     Object.keys(object).some(key => predicate(object[key], key, object));
+
+/**
+ *
+ * @param {Object} object
+ * @param {function(value: *, key: string, object: Object): boolean} predicate
+ * @return {Object}
+ */
+export const objectFilter = (object, predicate) => {
+    const ret = {};
+    objectForEach(object, (value, key, object) => {
+        if (predicate(value, key, object)) ret[key] = object[key];
+    });
+    return ret;
+};
+/**
+ *
+ * @param {*} value
+ * @return {*}
+ */
+export const clone = value => {
+    if (Array.isArray(value)) {
+        return value.map(clone);
+    }
+    else if (isObject(value)) {
+        return objectMap(value, clone);
+    }
+    else {
+        return value;
+    }
+};
 
 /**
  *
