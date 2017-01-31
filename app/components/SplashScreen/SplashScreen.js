d681bfc4643a755077e24c3c7fd6f99d7c886381
katya marova <marova@ordbuy.com>
2017-01-31 21:30:36 +0300

    splash screen

---
+++ app/components/SplashScreen/SplashScreen.js
@@ -0,0 +1,18 @@
+/**
+ * @author Ekaterina Marova
+ */
+
+'use strict';
+import React from 'react';
+import './SplashScreen.scss';
+
+export const SplashScreen = props => (
+  <div className="splash-screen">
+    <div className="splash-screen_image">
+      JSSY
+    </div>
+  </div>
+);
+
+SplashScreen.displayName = 'SplashScreen';
+
