a24196169ba36490068b264fa930833037df7d48
Dmitriy Bizyaev <bizyaev@ordbuy.com>
2017-05-18 19:57:25 +0300

    fixed https://trello.com/c/J20MXsBI/315-breadcrumbs

--- app/src/components/BlockContent/BlockBreadcrumbs/BlockBreadcrumbs.js
+++ app/src/components/BlockContent/BlockBreadcrumbs/BlockBreadcrumbs.js
@@ -4,9 +4,20 @@ import React from 'react';
 import { Breadcrumbs } from '@reactackle/reactackle';
 import './BlockBreadcrumbs.scss';
 
+/* eslint-disable react/prop-types */
+const LinkComponent = ({ className, onClick, children }) => (
+  <a className={className} style={{ cursor: 'pointer' }} onClick={onClick}>
+    {children}
+  </a>
+);
+/* eslint-enable react/prop-types */
+
 export const BlockBreadcrumbs = props => (
   <div className="block-breadcrumbs">
-    <Breadcrumbs {...props} />
+    <Breadcrumbs
+      {...props}
+      linkComponent={LinkComponent}
+    />
   </div>
 );
 
