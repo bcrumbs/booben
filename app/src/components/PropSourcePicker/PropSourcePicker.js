fca036e96e93a54dbcac8b0dfedb06c261a02ac6
Dmitriy Bizyaev <bizyaev@ordbuy.com>
2017-05-19 16:42:22 +0300

    fix for previous fix (=

--- app/src/components/PropSourcePicker/PropSourcePicker.js
+++ app/src/components/PropSourcePicker/PropSourcePicker.js
@@ -1,13 +1,40 @@
 'use strict';
 
-import React from 'react';
+import React, { Component } from 'react';
 import './PropSourcePicker.scss';
 
-export const PropSourcePicker = props => (
-  <div className="source-picker">
-    {props.children}
-  </div>
-);
+export class PropSourcePicker extends Component {
+  constructor(props, context) {
+    super(props, context);
+    
+    this._element = null;
+    
+    this._saveRef = this._saveRef.bind(this);
+    this._handleClick = this._handleClick.bind(this);
+  }
+  
+  componentDidMount() {
+    this._element.addEventListener('click', this._handleClick);
+  }
+  
+  _saveRef(ref) {
+    this._element = ref;
+  }
+  
+  _handleClick(event) {
+    event.stopPropagation();
+  }
+  
+  render() {
+    const { children } = this.props;
+    
+    return (
+      <div className="source-picker" ref={this._saveRef}>
+        {children}
+      </div>
+    );
+  }
+}
 
 PropSourcePicker.displayName = 'PropSourcePicker';
 
