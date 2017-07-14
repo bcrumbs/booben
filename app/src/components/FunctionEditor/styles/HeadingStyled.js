01c71b99eb205dda451528bad1f1efec86dcce53
Ekaterina Marova <marova@ordbuy.com>
2017-07-14 20:15:02 +0300

    done

--- app/src/components/FunctionEditor/styles/HeadingStyled.js
+++ app/src/components/FunctionEditor/styles/HeadingStyled.js
@@ -4,21 +4,31 @@ import styled from 'styled-components';
 
 import {
   baseModule,
-  paletteBlueGrey200,
+  paletteBlueGrey50,
+  colorBorder,
   textColorMedium,
 } from '../../../styles/themeSelectors';
 
 const codeMirrorOffset = 30; // CodeMirror ruler width + border
 
 export const HeadingStyled = styled.div`
-  background-color: ${paletteBlueGrey200};
+  background-color: ${paletteBlueGrey50};
   color: ${textColorMedium};
-  padding: ${baseModule}px ${codeMirrorOffset}px;
+  padding: ${baseModule(1)}px ${codeMirrorOffset}px;
   user-select: none;
 
   pre {
     padding: 0;
     margin: 0;
+  }
+  
+  &:first-of-type {
+    border-bottom: 1px solid ${colorBorder};
+  }
+  
+  &:last-of-type {
+    border-top: 1px solid ${colorBorder};
+    border-bottom: 1px solid ${colorBorder};
   }
 `;
 
