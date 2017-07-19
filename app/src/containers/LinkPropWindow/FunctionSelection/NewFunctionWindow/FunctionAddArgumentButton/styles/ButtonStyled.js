0b244181588a5ecb45533797f5f9fb5dcaea6b44
Ekaterina Marova <marova@ordbuy.com>
2017-07-19 15:42:29 +0300

    fix

--- app/src/containers/LinkPropWindow/FunctionSelection/NewFunctionWindow/FunctionAddArgumentButton/styles/ButtonStyled.js
+++ app/src/containers/LinkPropWindow/FunctionSelection/NewFunctionWindow/FunctionAddArgumentButton/styles/ButtonStyled.js
@@ -3,8 +3,7 @@
 import styled from 'styled-components';
 import { baseModule } from '../../../../../../styles/themeSelectors';
 
-export const ButtonStyled = styled.div`
-  
+export const ButtonStyled = styled.div`  
   & > *:first-child {
     margin-left: -${baseModule(0.5)}px;
   }
