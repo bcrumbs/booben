5c8b99c2ebccc9d0ce1d7d2e17889fe4edb76c4d
Katya Marova <marova.ekaterina@gmail.com>
2017-08-22 00:08:34 +0300

    wip

--- app/src/components/dataFlow/DataFlowBlock/DataFlowBlockItem/ArrayGroup/ArrayGroup.js
+++ app/src/components/dataFlow/DataFlowBlock/DataFlowBlockItem/ArrayGroup/ArrayGroup.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from '@reactackle/reactackle';
+import { Button, SelectBox } from '@reactackle/reactackle';
 import { ArrayGroupStyled } from './styles/ArrayGroupStyled';
 import { TitleStyled } from './styles/TitleStyled';
 import { ButtonRowStyled } from './styles/ButtonRowStyled';
 
 const propTypes = {
   title: PropTypes.string,
+  type: PropTypes.string,
 };
 
 const defaultProps = {
   title: 'idx',
+  type: 'string',
 };
 
+// TODO в селект запихиваем тип элементов массива
 export const ArrayGroup = props => (
   <ArrayGroupStyled>
+    <SelectBox />
     <TitleStyled>
       {props.title}
     </TitleStyled>
--- app/src/routes/DataFlow.draft.js
+++ app/src/routes/DataFlow.draft.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
   Button,
-  TextField
+  TextField,
+  SelectBox,
 } from '@reactackle/reactackle';
 
 import {
