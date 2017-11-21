91772ee57646cbc9184ff5524e2735e6db3789fe
nickmaltsev <maltsnikolay@gmail.com>
2017-11-21 08:53:32 +0500

    add placeholder to emptyList

--- app/src/components/CanvasFrame/CanvasFrame.js
+++ app/src/components/CanvasFrame/CanvasFrame.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { noop } from '../../utils/misc';
 import { CanvasFrameStyled } from './styles/CanvasFrameStyled';
 import { FrameStyled } from './styles/FrameStyled';
+import { CANVAS_IFRAME_ID } from '../../containers/builders/List/List';
 
 const propTypes = {
   iframeRef: PropTypes.func,
@@ -15,6 +16,7 @@ const defaultProps = {
 export const CanvasFrame = props => (
   <CanvasFrameStyled>
     <FrameStyled
+      id={CANVAS_IFRAME_ID}
       sandbox="allow-same-origin allow-scripts allow-pointer-lock"
       innerRef={props.iframeRef}
     />
--- app/src/containers/builders/List/List.js
+++ app/src/containers/builders/List/List.js
@@ -6,6 +6,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import patchComponent from '../../../hocs/patchComponent';
 import { returnNull } from '../../../utils/misc';
+import withOverlay from '../../../hocs/withOverlay';
 
 const propTypes = {
   data: PropTypes.array,
@@ -17,9 +18,47 @@ const defaultProps = {
   component: returnNull,
 };
 
-const _List = props => props.data.map((item, idx) => (
-  <props.component key={String(idx)} item={item} />
-));
+// TODO: where to move this?
+export const CANVAS_IFRAME_ID = 'CANVAS_IFRAME_ID';
+const getCanvasBody = () => {
+  const iframe = document.getElementById(CANVAS_IFRAME_ID);
+  const iframeDocument = iframe.contentDocument
+    ? iframe.contentDocument
+    : iframe.contentWindow.document;
+
+  return iframeDocument.body;
+};
+
+const EmptyListOverlay = ({ rect }) =>
+  <div
+    id="strange things"
+    style={{
+      position: 'fixed',
+      left: rect.left,
+      top: rect.top,
+      width: rect.width,
+      height: rect.height,
+      background: 'grey',
+      opacity: 0.7,
+      pointerEvents: 'none',
+    }}
+  >
+    text on overlay
+  </div>;
+
+const _List = props => {
+  if (!props.data.length) {
+    const canvasBody = getCanvasBody();
+    const ComponentWithOverlay = withOverlay({
+      OverlayComponent: EmptyListOverlay,
+      mountTarget: canvasBody,
+    })(props.component);
+    return <ComponentWithOverlay />;
+  }
+  return props.data.map((item, idx) => (
+    <props.component key={String(idx)} item={item} />
+  ));
+};
 
 _List.propTypes = propTypes;
 _List.defaultProps = defaultProps;
