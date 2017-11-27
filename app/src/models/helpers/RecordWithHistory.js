5cdc154895202511ae8f4cd4e44a04f57e7c21d6
Vladimir Nadygin <VNadygin@me.com>
2017-11-27 23:31:32 +0400

    Fix

--- app/src/models/helpers/RecordWithHistory.js
+++ app/src/models/helpers/RecordWithHistory.js
@@ -21,7 +21,6 @@ export default (
   
   return class extends RecordClass {
     _extractHistoryEntry() {
-      console.log('historyProps', historyProps)
       return arrayToObject(
         historyProps,
         returnArg,
@@ -31,25 +30,15 @@ export default (
     
     _applyHistoryEntry(historyEntry) {
       let record = this;
-      const {
-        data,
-        indexRouteSelected,
-        lastComponentId,
-        lastRouteId,
-        selectedRouteId,
-        designer,
-      } = unflatten(historyEntry);
-     
-      record = record.setIn(['data'], data);
-      record = record.setIn(['indexRouteSelected'], indexRouteSelected);
-      record = record.setIn(['lastComponentId'], lastComponentId);
-      record = record.setIn(['lastRouteId'], lastRouteId);
-      record = record.setIn(['selectedRouteId'], selectedRouteId);
-      record = record.setIn(
-        ['designer', 'selectedComponentIds'],
-        designer.selectedComponentIds,
-      );
 
+      historyProps.forEach(prop => {
+        const path = prop.split('.');
+        const value = prop.split('.')
+          .reduce((previous, current) =>
+            previous[current], unflatten(historyEntry));
+        record = record.setIn(path, value);
+      });
+      
       return record;
     }
   
