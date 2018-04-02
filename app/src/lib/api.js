d1d751a64b1dbbc56b1fa171914ef3477913cf7a
Vladimir Nadygin <vnadygin@vnadygin-macbook.local>
2018-04-02 19:46:38 +0400

    WIP: auth

--- app/src/lib/api.js
+++ app/src/lib/api.js
@@ -1,5 +1,9 @@
+import Cookie from 'js-cookie';
 import { introspectionQuery } from 'graphql/utilities';
 import { URL_API_PREFIX } from '../../../shared/constants';
+
+const backendUrl =
+      'https://nr0pmvrpw6.execute-api.eu-central-1.amazonaws.com/production/';
 
 /**
  *
@@ -8,12 +12,17 @@ import { URL_API_PREFIX } from '../../../shared/constants';
  */
 export const getProject = async projectName => {
   let url = URL_API_PREFIX;
+  const jwt = Cookie.getJSON('jssy-jwt');
 
   if (process.env.NODE_ENV === 'production') {
-    url = 'https://32dj7c5axd.execute-api.eu-central-1.amazonaws.com/dev';
+    url = backendUrl;
   }
 
-  const res = await fetch(`${url}/projects/${projectName}`);
+  const res = await fetch(`${url}/projects/${projectName}`, {
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  });
   const data = await res.json();
 
   if (data.error) throw new Error(data.error);
@@ -31,13 +40,15 @@ export const putProject = async (projectName, project) => {
   let url = URL_API_PREFIX;
 
   if (process.env.NODE_ENV === 'production') {
-    url = 'https://32dj7c5axd.execute-api.eu-central-1.amazonaws.com/dev';
+    url = backendUrl;
+    const jwt = Cookie.getJSON('jssy-jwt');
 
     const res = await fetch(`${url}/projects/${projectName}`, {
       method: 'POST',
       body: JSON.stringify(project),
       headers: {
         'content-type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
       },
     });
     
