3cc6f35e9ba01c0cd032cd3598af8151c0887660
Vladimir Nadygin <vnadygin@vnadygin-macbook.local>
2018-04-08 18:00:59 +0400

    error fix

--- app/src/lib/api.js
+++ app/src/lib/api.js
@@ -24,8 +24,8 @@ export const getProject = async projectName => {
     },
   });
   const data = await res.json();
-
   if (data.error) throw new Error(data.error);
+  if (data.message) throw new Error(data.message);
   return data;
 };
 
@@ -68,6 +68,7 @@ export const putProject = async (projectName, project) => {
     const data = await res.json();
   
     if (data.error) throw new Error(data.error);
+    if (data.message) throw new Error(data.message);
     return data;
   }
 };
