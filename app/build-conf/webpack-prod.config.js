7cef4cd472ee78671666897fa0a036b096cdf080
Vladimir Nadygin <vnadygin@me.com>
2018-08-09 17:29:54 +0300

    minify code fix

--- app/build-conf/webpack-prod.config.js
+++ app/build-conf/webpack-prod.config.js
@@ -184,5 +184,25 @@ module.exports = {
     new CopyWebpackPlugin([
       { from: 'strings', to: 'strings' },
     ]),
+
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false
+      }
+    }),
+    
+    new webpack.HashedModuleIdsPlugin(),
   ],
 };
