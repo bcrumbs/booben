ed927a81d66b6e12c500adc0e91888f40eebb95c
D1abloRUS <sergey.monakhov@gmail.com>
2018-08-10 22:08:57 +0300

    fix docker build

--- docker/travis-deploy.sh
+++ docker/travis-deploy.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-make project-build DEPLOY="travis"
+make project-build DEPLOY="demo"
 make docker-build
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker tag booben:latest $DOCKER_REPO:demo
+docker push $DOCKER_REPO:demo
+docker rmi booben:latest
+
+make project-build
+make docker-build
 docker tag booben:latest $DOCKER_REPO:latest
 docker push $DOCKER_REPO:latest
 docker tag $DOCKER_REPO:latest $DOCKER_REPO:"$(sed -nr 's/.*"version":\ "(.*)",/\1/p' package.json)"
